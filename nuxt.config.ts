3161993a29e0478574d9fe82712ebb31f11f7590
Ali Sadeghi <alisadghi228@gmail.com>
2024-01-02 21:54:46 +0330

    feat : config nuxt config and install dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: process.env.npm_package_name || "",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: process.env.npm_package_description || "",
+        },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css",
+        },
+      ],
+    },
+  },
+  css: ["@assets/css/style.css"],
+  plugins: [],
+  components: true,
+  modules: ["bootstrap-vue/nuxt"],
+});
